57390f8e2d7260ee9adc1f7cc2023d551c75a7d7
robbie-jack <r0bsterj29@gmail.com>
2023-06-11 11:59:14 +0100

    Removed old shaders & CMakeLists.txt
    Also removed an unneeded compile.bat file.

--- content/shaders/compile.bat
+++
@@ -1,5 +0,0 @@
-C:\VulkanSDK\1.3.216.0\Bin\glslc.exe shader.vert -o vert.spv
-C:\VulkanSDK\1.3.216.0\Bin\glslc.exe shader.frag -o frag.spv
-C:\VulkanSDK\1.3.216.0\Bin\glslc.exe shadowmap.vert -o shadowmap_vert.spv
-C:\VulkanSDK\1.3.216.0\Bin\glslc.exe shadowmap.frag -o shadowmap_frag.spv
-pause
